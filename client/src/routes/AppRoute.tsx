6a07130ffd3a010e7dd4decf6f47e3c6a94f8c91
DanishVahora <danishvhora786@gmail.com>
2025-05-08 02:19:18 +0530

    Added Buyer side pages

--- client/src/routes/AppRoute.tsx
+++ client/src/routes/AppRoute.tsx
@@ -1,7 +1,7 @@
 
 import { Routes, Route } from 'react-router-dom';
 import SupplierRegister from '../pages/RegisterSupplier';
-import BuyerRegister from '../pages/RegisterBuyer';
+import BuyerRegister from '../pages/buyer/RegisterBuyer';
 import LandingPage from '../pages/LandingPage';
 import SellerLayout from '../layout/seller/SellerLayout';
 import SellerDashboard from '../pages/seller/SellerDashboard';
@@ -9,6 +9,10 @@ import UploadInvoice from '../pages/seller/UploadInvoice';
 import BidsAndOffers from '../pages/seller/BidsAndOffers';
 import FinancierRegister from '../pages/RegisterFinancier';
 import VerificationPage from '../pages/VerificationPage';
+import BuyerLayout from '../layout/buyer/BuyerLayout';
+import BuyerDashboard from '../pages/buyer/BuyerDashboard';
+import InvoiceApproval from '../pages/buyer/InvoiceApproval';
+import InvoiceStatusPage from '../pages/buyer/InvoiceStatus';
 
 export const AppRoute = () => (
   <Routes>
@@ -26,6 +30,15 @@ export const AppRoute = () => (
       {/* <Route path="invoices" element={<div>Invoices Page</div>} /> */}
       <Route path="settings" element={<div>Settings Page</div>} />
     </Route>
+
+    {/* Buyer Routes */}
+    <Route path="/buyer" element={<BuyerLayout />}>
+      <Route path="dashboard" element={<BuyerDashboard/>} />
+      <Route path="approvals" element={<InvoiceApproval/>}/>
+      <Route path="invoice-status" element={<InvoiceStatusPage/>}></Route>
+    </Route>
+
+    
 
     <Route path="*" element={<LandingPage />} />
   </Routes>
